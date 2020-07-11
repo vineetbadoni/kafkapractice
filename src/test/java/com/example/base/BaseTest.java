43a843386e34bfeb5b3a08e1e386abe890173469
vineetbadoni <vineetbadoni@gmail.com>
2020-07-12 05:11:09 +0530

    Fixed - Multiple Test cases running on the same Test Container

--- src/test/java/com/example/base/BaseTest.java
+++ src/test/java/com/example/base/BaseTest.java
@@ -1,34 +1,28 @@
 package com.example.base;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.containers.Network;
-import org.testcontainers.containers.wait.strategy.Wait;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
-public class BaseTest {
+public abstract class BaseTest {
 
     private static Network network = Network.newNetwork();
 
-    @ClassRule
-    @Container
-    public static final GenericContainer MY_SQL_CONTAINER =
-            new MySQLContainer()
-            .withDatabaseName("arbl")
-            .withUsername("arbl")
-            .withPassword("arbl")
-            .withNetwork(network)
-            .withExposedPorts(3306)
-            .waitingFor(Wait.forListeningPort());
+    public static final GenericContainer MY_SQL_CONTAINER;
+    static {
+        MY_SQL_CONTAINER = new MySQLContainer()
+                .withDatabaseName("arbl")
+                .withUsername("arbl")
+                .withPassword("arbl")
+                .withNetwork(network)
+                .withExposedPorts(3306);
 
-    @BeforeClass
-    public static void startContainers() {
-        System.out.println("Started MySQL container");
+        //Start the container. Blocking call.
+        MY_SQL_CONTAINER.start();
+
+        //Set the parameters required to connect to MySQL.
         System.setProperty("DB_PORT",MY_SQL_CONTAINER.getMappedPort(3306)+"");
         System.setProperty("DB_USER","arbl");
         System.setProperty("DB_PASSWORD","arbl");
