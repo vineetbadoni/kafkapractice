78dc56f99ba8d7650dc9cecdbcff775ba6053fed
vineetbadoni@gmail.com <vineetbadoni@gmail.com>
2020-07-16 10:51:07 +0530

    Working example with (1) 2 BDD tests (2) JSON driven, Input and Output is provided via JSON (3) Downloading and Running containers only (4) Proves how an API fails when the response changes

--- src/test/java/com/example/payroll/EmployeeControllerTest_With_BDD_REST_Mocks_2.java
+++ src/test/java/com/example/payroll/EmployeeControllerTest_With_BDD_REST_Mocks_2.java
@@ -1,6 +1,5 @@
 package com.example.payroll;
 
-import com.example.DemoApplication;
 import com.example.base.ZerocodeSpringBootRunner;
 import org.jsmart.zerocode.core.domain.Scenario;
 import org.jsmart.zerocode.core.domain.TargetEnv;
@@ -14,7 +13,7 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 @TargetEnv("application_host.properties")
 @RunWith(ZerocodeSpringBootRunner.class)
-public class EmployeeControllerTest_With_BDD_REST_Mocks {
+public class EmployeeControllerTest_With_BDD_REST_Mocks_2 {
     @Test
     @Scenario("integration_tests/country/get/get_all_employees_by_id_test.json")
     public void all() throws Exception{}
